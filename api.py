2553928f9eecc0836ed2c0329e8d99f619c749de
Louis-AI-dev <cotinat.louis@gmail.com>
2025-06-16 10:48:01 +0100

    Ajout de la classe dans api.py

--- api.py
+++ api.py
@@ -10,9 +10,29 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from custom_transformers import CleanAndStemTweets
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#from custom_transformers import CleanAndStemTweets
+import re
 
+class CleanAndStemTweets(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+        self.stop_words = set(stopwords.words("english"))
+        self.stemmer = PorterStemmer()
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        cleaned = []
+        for tweet in X:
+            tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet)
+            tokens = self.tokenizer.tokenize(tweet)
+            tokens = [t for t in tokens if t.isalpha() and t not in self.stop_words]
+            stems = [self.stemmer.stem(t) for t in tokens]
+            cleaned.append(' '.join(stems))  # Option : retourne une chaîne pour compatibilité tokenizer
+        return cleaned
+    
 # Configuration du logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
